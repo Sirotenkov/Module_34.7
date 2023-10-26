a68c648e11ed3889ad59a3eac2ea576a35db862e
Сиротёнков Алексей Юрьевич <a.y.sirotyonkov@corp.lis-dspr.ru>
2023-10-26 16:46:14 +0300

    Add files

---
+++ Module_34.7.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+
+template<typename ty>
+concept ComplexConcept = requires(ty t) {
+	{t.toString()} -> std::same_as<std::string>;
+	{t.hash()} -> std::convertible_to<long>;
+
+} && !std::has_virtual_destructor<ty>::value;
+
+class foo {
+public:
+	std::string toString() { return {}; }
+	int hash() { return {}; }
+};
+
+// В классе объявлен виртуальный деструктор, поэтому ограничение, накладываемое концептом, не выполнено
+// Это подсвечивает IDE и компилятор выдаёт соответствующее сообщение
+// Если убрать слово virtual, т.е. сделать деструктор не виртуальным, то ограничение концепта будет выполнено
+// и программа отработает без ошибок
+
+class bar {
+public:
+	virtual ~bar() {}
+	std::string toString() { return {}; }
+	int hash() { return {}; }
+};
+
+static_assert(ComplexConcept<foo>);
+static_assert(ComplexConcept<bar>);
+
+int main() {
+	return 0;
+}
